c36889c5af6a8ca3edebd9af0e282012eca593ee
Gaël <gael.giraud@atsys.fr>
2020-08-17 14:12:28 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,8 @@ opts=(
 	dc_local_interfaces "[${BIND_IP:-0.0.0.0}]:${PORT:-25} ; [${BIND_IP6:-::0}]:${PORT:-25}"
 	dc_other_hostnames "${OTHER_HOSTNAMES}"
 	dc_relay_nets "$(ip addr show dev eth0 | awk '$1 == "inet" { print $2 }' | xargs | sed 's/ /:/g')${RELAY_NETWORKS}"
+	helo_accept_junk_hosts '!*'
+	helo_allow_chars '_'
 )
 
 if [ "$DISABLE_IPV6" ]; then
